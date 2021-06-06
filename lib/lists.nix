fe6bcdf470d27510815b6612348d480443fe2e3c
GTrunSec <GTrunSec@users.noreply.github.com>
2021-06-06 21:49:44 +0000

    deploy: 0f72f26a5750502ed505a57090b55dc53b879191

--- lib/lists.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }:
-{
-  pathsIn = dir:
-    let
-      fullPath = name: "${toString dir}/${name}";
-    in
-    map fullPath (lib.attrNames (lib.optionalAttrs
-      (builtins.pathExists dir) (builtins.readDir dir)));
-}
